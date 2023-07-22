017b913f292ebad1acaa188863b9d27ff8b2c863
Shu Li <shu.li@outlook.com>
2023-07-22 10:18:05 -0400

    read TSV sample

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from st_files_connection import FilesConnection
 conn = st.experimental_connection('s3', type=FilesConnection)
 data = conn.read('amazon-reviews-pds/tsv/sample_us.tsv', input_format='text')
 #data = conn.read('amazon-reviews-pds/parquet/product_category=Software/part-00000-495c48e6-96d6-4650-aa65-3c36a3516ddd.c000.snappy.parquet', input_format='parquet')
-st.write(data.head())
+st.write(data)
